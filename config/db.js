6baf5ee8088b9dc715d7112735e6425f3e6968d7
sowetowp <ayodejiamzat@gmil.com>
2022-10-24 13:06:25 +0100

    first push on my backend

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 const connectDB = async () => {
 	try {
-		const conn = await mongoose.connect(process.env.MONGO_TEST, {
+		const conn = await mongoose.connect("mongodb+srv://sowetowp:08138226965@cluster0.qtqkvxz.mongodb.net/sowetowp?retryWrites=true&w=majority", {
 			useUnifiedTopology: true,
 			useNewUrlParser: true,
 			//useCreateIndex: true,
