b6f022d51af6503e7919bf1e1e06fa183ec2fe29
sowetowp <ayodejiamzat@gmil.com>
2022-12-04 15:13:13 +0100

    malik

--- server.js
+++ server.js
@@ -23,7 +23,13 @@ connectDB().then()
 const app = express()
 
 app.use(morgan("dev"))
-app.use(cors())
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
+
 app.use(express.json())
 
 app.use(errorHandler)
