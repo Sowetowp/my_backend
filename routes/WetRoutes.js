4dc2d8184a20af6de9e2053e8b179d29e7e47081
sowetowp <ayodejiamzat@gmil.com>
2022-12-04 03:48:52 +0100

    malik

--- routes/WetRoutes.js
+++ routes/WetRoutes.js
@@ -2,8 +2,10 @@ import express from "express";
 import { wet_sign_in, wet_sign_up } from "../controllers/WetController.js";
 import { wetProtect } from "../middlewares/auth_handlers.js";
 const wet_router = express.Router()
-wet_router.route("/")
+wet_router.route("/up")
     .post(wet_sign_up)
+    
+wet_router.route("/in")
     .get(wet_sign_in)
 
 
